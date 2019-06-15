a0f2ab538a1cfa2cff6763f620edfc34268e05c2
Ibee TechTest <ibeeatta@gmail.com>
2019-06-15 07:17:40 +0100

    Debug mode to False

--- techtest/settings.py
+++ techtest/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8#t2btjobik15tv20a#h9duxe_g_e93-7oflf0r)*u!2+50mo('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
